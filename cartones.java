5f15be103d618c47f60f99a7089e1afe4604bc42
Raojayou <jose_10tonio@hotmail.com>
2017-01-12 19:05:21 +0100

    Generacion de Número de cartones
    Código que pregunta al usuario cuántos cartones quiere y lo imprime por
    pantalla.

---
+++ cartones.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+public class cartones {
+
+    public static void main(String[] args) {
+        // Variables
+        Scanner input = new Scanner(System.in);
+        int cartones;
+
+        System.out.println("Introduzca el número de cartones que desea: ");
+        cartones = input.nextInt();
+
+        int numeroCartones[]= new int[cartones];
+    }
+}
