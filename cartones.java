5b667419eed4d4153f2fb5e1a2d8797a1fddb8cd
AdryDev92 <tazzmaniac51@gmail.com>
2017-01-13 20:40:19 +0100

    Actualizado cartones.java
    Resuelta la parte de generar un cartón con los 15 números. Hay que resolver como generar cualquier número de cartones.

--- cartones.java
+++ cartones.java
@@ -11,5 +11,24 @@ public class cartones {
         cartones = input.nextInt();
 
         int numeroCartones[]= new int[cartones];
+        
+            System.out.println("Has seleccionado " + "[" + cartones + "]" + " carton/es");
+
+            System.out.println("\n!Empieza el bingo!");
+
+            int CartonesNumeros[][] = new int [3][5];
+
+            for (int i = 0; i < CartonesNumeros.length; i++) {
+                for (int j = 0; j < CartonesNumeros[i].length; j++) {
+                    CartonesNumeros[i][j] = (int)(1+Math.random()*90);
+
+                   System.out.printf("%3d ",CartonesNumeros[i][j]);
+                }
+                System.out.println();
+            }
+
+        }
+
+    }
     }
 }
