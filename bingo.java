1f6d770d56099b04b7ec5d52a13c12452cdb962b
Albertomb11 <alberto1sevilla@hotmail.com>
2017-02-17 17:28:40 +0100

    Update bingo.java
    Es es el nuevo

--- bingo.java
+++ bingo.java
@@ -1,15 +1,17 @@
 import java.util.*;
-import java.util.Arrays;
 
-public class bingo {
+public class Bingo {
 
     public static void main(String[] args) {
+
         Scanner input=new Scanner(System.in);
 
+        //Variables
         int cartones[][];
         int numCarton;
         int numRandom = 0;
 
+        //Preguntamos cuantos cartones va a querer para jugar
         System.out.print("Introduce el numero de cartones: ");
         numCarton = input.nextInt();
         cartones = new int[numCarton][15];
@@ -21,14 +23,21 @@ public class bingo {
         sortArray(cartones);
         printArray(cartones);
 
+        //metodo para cantar la linea y el bingo
+        checkLinea();
+
         //metodo para la generación de bolas
         GenerateBalls();
-        checkCartones(cartones);
+
+
+        //checkBingo(cartones);
+        //poner0Array(cartones);
 
     }
 
+
     /**
-     *
+     * Generamos los numero del 1 al 90.
      * @param num
      * @return
      */
@@ -38,7 +47,7 @@ public class bingo {
     }
 
     /**
-     *
+     * Rellena los arrays para los cartones
      * @param cartones
      */
     static void fillArray(int cartones[][]) {
@@ -58,6 +67,10 @@ public class bingo {
         }
     }
 
+    /**
+     * checkeamos el array para que no se repita los numeros
+     * @param arrayCarton
+     */
     static void CheckArray(int arrayCarton[][]){
         int num = 0;
         boolean check = false;
@@ -76,6 +89,13 @@ public class bingo {
 
     }
 
+    /**
+     * Checkeamos los numeros de los cartones.
+     * @param arrayCarton
+     * @param num
+     * @param NCarton
+     * @return
+     */
     static boolean checknumber(int arrayCarton[][], int num, int NCarton){
         boolean comprobado = false;
 
@@ -88,7 +108,7 @@ public class bingo {
     }
 
     /**
-     *
+     * Ordenamos los numeros de menor a mayor para los cartones
      * @param arrayOrdenar
      */
     static void sortArray(int arrayOrdenar[][]) {
@@ -99,14 +119,14 @@ public class bingo {
     }
 
     /**
-     *
+     * Imprimimos los cartones en tres filas de cinco numeros
      * @param arrayImprimir
      */
     static void printArray(int arrayImprimir[][]) {
         int numerocarton = 0;
 
         for (int i = 0; i < arrayImprimir.length; i++) {
-            System.out.println("Carton " + i);
+            System.out.println("Carton " + (i+1));
             for (int j = 0; j < arrayImprimir[0].length; j++) {
                 System.out.print(arrayImprimir[i][j] + " ");
                 numerocarton++;
@@ -119,23 +139,33 @@ public class bingo {
         }
     }
 
+    /**
+     * Generamos las bolas aleatoriamente del 1 al 90.
+     * @return
+     */
     static int GenerateBalls(){
         int bolas = 0;
         int almacenarBolas [] = new int [90];
 
         for (int i = 0; i < almacenarBolas.length; i++) {
-            
-                do {
-                    bolas = (int)(Math.random()*90+1);
-                }while (checkBolas(almacenarBolas, bolas));
+
+            do {
+                bolas = (int)(Math.random()*90+1);
+            }while (checkBolas(almacenarBolas, bolas));
             almacenarBolas[i] = bolas;
-            
-            System.out.println("\n"+almacenarBolas[i]);
+
+            System.out.println("Bola: "+"\n"+almacenarBolas[i]);
         }
 
         return bolas;
     }
-    
+
+    /**
+     * Utilizamos este metodo para meter las bolas generadas en un array
+     * @param arrayGenerico
+     * @param numBola
+     * @return
+     */
     static boolean checkBolas(int arrayGenerico[], int numBola){
 
         for (int i = 0; i < arrayGenerico.length; i++) {
@@ -146,10 +176,25 @@ public class bingo {
         return false;
     }
 
-    static boolean checkCartones(int Cartones[][]){
+    static boolean checkLinea(int cartones[][], int almacenarBolas[]) {
+        boolean checkLinea = false;
 
-        
-
-
+        for (int e = 0; e < almacenarBolas.length; e++) {
+            for (int i = 0; i < cartones.length; i++) {
+                for (int j = 0; j < cartones[i].length; j++) {
+                    if (almacenarBolas[e] == cartones[i][j]) {
+                        cartones[i][j] = 0;
+                        if (cartones[i][0] == 0 && cartones[i][1] == 0 && cartones[i][2] == 0 && cartones[i][3] == 0 && cartones[i][4] == 0) {
+                            checkLinea = true;
+                        } else if (cartones[i][5] == 0 && cartones[i][6] == 0 && cartones[i][7] == 0 && cartones[i][8] == 0 && cartones[i][9] == 0) {
+                            checkLinea = true;
+                        } else if (cartones[i][10] == 0 && cartones[i][11] == 0 && cartones[i][12] == 0 && cartones[i][13] == 0 && cartones[i][14] == 0) {
+                            checkLinea = true;
+                        }
+                    }
+                }
+            }
+        }
+        return true;
     }
 }
